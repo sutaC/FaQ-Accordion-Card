aba32bbef7d6f9c9ff06f31bb1025b58978ef837
Antoni Siekański <catus.pl@gmail.com>
2023-03-22 10:31:26 +0100

    Html layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,63 @@
+<script>
+    import Faq from "./faq.svelte"
 
-How many team members can I invite?
-You can invite up to 2 additional users on the Free plan. There is no limit on 
-team members for the Premium plan.
 
-What is the maximum file upload size?
-No more than 2GB. All files in your account must fit your allotted storage space.
+</script>
+<!-- ---  -->
 
-How do I reset my password?
-Click “Forgot password” from the login page or “Change password” from your profile page.
-A reset link will be emailed to you.
+<div class="card">
+    <div class="images">
+        <img src="/illustration-box-desktop.svg" alt="illustration-box">
+        <img src="/illustration-woman-online-desktop.svg" alt="illustration-woman-online">
+    </div>
+    <div class="questions">
 
-Can I cancel my subscription?
-Yes! Send us a message and we’ll process your request no questions asked.
+        <h1>FAQ</h1>
 
-Do you provide additional support?
-Chat and email support is available 24/7. Phone lines are open during normal business hours.
+        <Faq>
+            <svelte:fragment slot="question">How many team members can I invite?</svelte:fragment>
+            <svelte:fragment slot="answer">You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</svelte:fragment>
+        </Faq>
+        
+        <Faq>
+            <svelte:fragment slot="question">What is the maximum file upload size?</svelte:fragment>
+            <svelte:fragment slot="answer">No more than 2GB. All files in your account must fit your allotted storage space.</svelte:fragment>
+        </Faq>
+        
+        <Faq>
+            <svelte:fragment slot="question">How do I reset my password?</svelte:fragment>
+            <svelte:fragment slot="answer">Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.</svelte:fragment>
+        </Faq>
+        
+        <Faq>
+            <svelte:fragment slot="question">Can I cancel my subscription?</svelte:fragment>
+            <svelte:fragment slot="answer">Yes! Send us a message and we’ll process your request no questions asked.</svelte:fragment>
+        </Faq>
+        
+        <Faq>
+            <svelte:fragment slot="question">Do you provide additional support?</svelte:fragment>
+            <svelte:fragment slot="answer">Chat and email support is available 24/7. Phone lines are open during normal business hours.</svelte:fragment>
+        </Faq>
+    </div>
+</div>
 
-<div class="attribution">
+
+
+<footer>
   Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
   Coded by <a href="#sutaC">Your Name Here</a>.
-</div>
+</footer>
+
+<!-- ---  -->
+<style>
+
+
+    footer {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+        font-size: 10px;
+    }
+
+</style>
---
+++ src/routes/faq.svelte
@@ -0,0 +1,7 @@
+
+
+<div class="card">
+    <h2><slot name="question"/></h2>
+    <p><slot name="answer"/></p>
+    <img src="/icon-arrow-down.svg" alt="icon-arrow-down">
+</div>
